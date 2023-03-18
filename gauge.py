cdfceca98878dfb23f32c4c45b3e81ba7b54044f
brendan360 <brendan360@hotmail.com>
2023-03-18 21:55:00 +1100

    ...

--- gauge.py
+++ gauge.py
@@ -11,17 +11,25 @@ import pygame
 # initiate pygame and give permission
 # to use pygame's functionality.
 pygame.init()
+X = 480
+Y = 480
  
 # define the RGB value for white,
 #  green, blue colour .
 white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
- 
-# assigning values to X and Y variable
-X = 480
-Y = 480
- 
+
+
+
+
+def loading():
+    imp = pygame.image.load("./logo.jpg").convert() 
+    display_surface.blit(imp, (0, 0))    
+    pygame.display.flip()
+
+
+
 # create the display surface object
 # of specific dimension..e(X, Y).
 display_surface = pygame.display.set_mode((X, Y))
@@ -45,20 +53,9 @@ textRect = text.get_rect()
  
 # set the center of the rectangular object.
 textRect.center = (X // 2, Y // 2)
- 
-# infinite loop
+display_surface.fill(white)
+display_surface.blit(text, textRect)
+time.sleep(5)
 
 
-
-def loading():
-    display_surface.fill(white)  
-    display_surface.blit(text, textRect)
-    time.sleep(5)
-    imp = pygame.image.load("./logo.jpg").convert() 
-    display_surface.blit(imp, (0, 0))    
-    pygame.display.flip()
-
-
-
-
-loading();
+loading()

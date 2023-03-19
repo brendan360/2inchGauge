5da89b2a7527d288d4299fac5cc65e8b9da1a0f2
brendan360 <brendan360@hotmail.com>
2023-03-19 14:27:43 +1100

    .

--- gauge.py
+++ gauge.py
@@ -160,11 +160,11 @@ def functHIGHLIGHTBOOTDISPLAY():
         display_surface.blit(text, textRect)
         text = Loadingfont.render(".....", True,white, black)
         textRect = text.get_rect()
-        textRect.center = (X // 2, Y // 2 -20)
+        textRect.center = (180, Y // 2 -20)
         display_surface.blit(text, textRect)
         text = Loadingfont.render(faildot, True, red, black)
         textRect = text.get_rect()
-        textRect.center = (X // 2, Y // 2 -20)
+        textRect.center = (180, Y // 2 -20)
         display_surface.blit(text, textRect)
         pygame.display.update()
         if bootState['obd'][0]==5:
@@ -182,7 +182,7 @@ def functHIGHLIGHTBOOTDISPLAY():
         display_surface.blit(text, textRect)
         text = Loadingfont.render(faildot, True, green, black)
         textRect = text.get_rect()
-        textRect.center = (X // 2, Y // 2 -20)
+        textRect.center = (180, Y // 2 -20)
         display_surface.blit(text, textRect)
         pygame.display.update()
        

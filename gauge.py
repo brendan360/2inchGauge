b334686191781147777626f4e979da89c866fba9
brendan360 <brendan360@hotmail.com>
2023-03-20 21:36:00 +1100

    .

--- gauge.py
+++ gauge.py
@@ -269,7 +269,7 @@ def functDISPLAYGAUGE(pid,low,high,warning,postfix):
 
 
     center = (X // 2, Y // 2)
-    radius = 100
+    radius = 200
     thickness = 50
     start_angle = 3* math.pi / 4
     end_angle = 9 * math.pi / 4

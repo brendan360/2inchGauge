8de1ac4b5261c99aa91fe186315dc6296d00cab0
brendan360 <brendan360@hotmail.com>
2023-03-21 22:16:46 +1100

    ..

--- gauge.py
+++ gauge.py
@@ -292,8 +292,8 @@ def functDISPLAYGAUGE(pid,low,high,warning,postfix,percentage):
     pygame.draw.arc(display_surface, gauge_color, (center[0]-radius, center[1]-radius, radius*2, radius*2), start_angle, start_angle + angle_range, thickness)
     index_length = 20
     index_thickness = 2
-    pygame.draw.line(screen, (0, 0, 0), (center[0], center[1] - radius), (center[0], center[1] - radius - index_length), index_thickness)
-    pygame.draw.line(screen, (0, 0, 0), (center[0], center[1] + radius), (center[0], center[1] + radius + index_length), index_thickness)
+    pygame.draw.line(display_surface, (0, 0, 0), (center[0], center[1] - radius), (center[0], center[1] - radius - index_length), index_thickness)
+    pygame.draw.line(display_surface, (0, 0, 0), (center[0], center[1] + radius), (center[0], center[1] + radius + index_length), index_thickness)
 
 
 # Draw the center circle

da8ec527ecc7b6c384dda3350f65f7ddd1878010
Brendan <brendan360@hotmail.com>
2023-03-21 15:41:14 +1100

    .

--- gauge.py
+++ gauge.py
@@ -313,8 +313,8 @@ def functDISPLAYGAUGE(pid,low,high,warning,postfix,percentage):
     text2 = font.render("80%", True, black)
     text1_rect = text1.get_rect(center=(center[0] + radius * math.cos(-math.pi / 4), center[1] - radius * math.sin(-math.pi / 4)))
     text2_rect = text2.get_rect(center=(center[0] + radius * math.cos(-math.pi * 5 / 4), center[1] - radius * math.sin(-math.pi * 5 / 4)))
-    screen.blit(text1, text1_rect)
-    screen.blit(text2, text2_rect)
+    display_surface.blit(text1, text1_rect)
+    display_surface.blit(text2, text2_rect)
 
     pygame.display.update()
 

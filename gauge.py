dae6279cc7857b6b5b486c36aac932513016f7f2
brendan360 <brendan360@hotmail.com>
2023-03-26 21:35:27 +1100

    ...

--- gauge.py
+++ gauge.py
@@ -313,9 +313,9 @@ def functDISPLAYGAUGE(gaugeItem):
     input_value = gaugeItems[gaugeItem][3]
 
 # Determine gauge color based on input value
-    if input_value < gaugeItems[gaugeItem][8]:
+    if input_value < int(gaugeItems[gaugeItem][8]):
         gauge_color = blue
-    elif input_value < gaugeItems[gaugeItem][9]:
+    elif input_value < int(gaugeItems[gaugeItem][9]):
         gauge_color = green
     else: 
         gauge_color = red
